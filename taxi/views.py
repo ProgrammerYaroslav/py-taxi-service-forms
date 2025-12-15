28afc983f2d74a6631d5e6c7d75d585c60f9b02a
Yaroslav <yaroslav.liubarets@icloud.com>
2025-12-15 18:28:03 +0000

    Solution 1.1

--- taxi/views.py
+++ taxi/views.py
@@ -14,18 +14,18 @@ class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
     model = Manufacturer
     form_class = ManufacturerForm
     success_url = reverse_lazy("taxi:manufacturer-list")
-    template_name = "taxi/manufacturer_form.html"
+    # template_name removed (inferred as taxi/manufacturer_form.html)
 
 class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Manufacturer
     form_class = ManufacturerForm
     success_url = reverse_lazy("taxi:manufacturer-list")
-    template_name = "taxi/manufacturer_form.html"
+    # template_name removed
 
 class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Manufacturer
     success_url = reverse_lazy("taxi:manufacturer-list")
-    template_name = "taxi/manufacturer_confirm_delete.html"
+    # template_name removed (inferred as taxi/manufacturer_confirm_delete.html)
 
 
 # --- Car Views ---
@@ -34,15 +34,15 @@ class CarCreateView(LoginRequiredMixin, generic.CreateView):
     model = Car
     form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
-    template_name = "taxi/car_form.html"
+    # template_name removed
 
 class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Car
     form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
-    template_name = "taxi/car_form.html"
+    # template_name removed
 
 class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Car
     success_url = reverse_lazy("taxi:car-list")
-    template_name = "taxi/car_confirm_delete.html"
+    # template_name removed
